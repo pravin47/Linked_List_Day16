0bcb69206da2adb0ec3e52d0aa06f710e94061f4
pravin bagul <bagul.pravin2@gmail.com>
2021-02-27 18:45:12 +0530

    Sorted_Link_List

--- Linked_List.java
+++ Linked_List.java
@@ -1,21 +1,47 @@
-import org.junit.Assert;
-import org.junit.Test;
-
-public class MyStackTest {
-
+public class SortedLinkedList  {
+	public INode head;
+	public INode tail;
 	
+	public SortedLinkedList(){
+		this.head = null;
+	}
 	
-	@Test
-	public void given3NumbersInStack_WhenPoped_ShouldMatchLastAddedNode() {
-		MyNode<Integer> myFirstNode = new MyNode<>(70);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(56);
-		MyStack myStack = new MyStack();
-		myStack.push(myFirstNode);
-		myStack.push(mySecondNode);
-		myStack.push(myThirdNode);
-		myStack.popTillEmpty();
-		Assert.assertEquals(myStack.count,0);
+	public <K extends Comparable<K>> void sortedInsert (INode newNode) {
+		INode current = this.head;
+		INode previous = null;
+		while(current != null && (((Comparable<K>) newNode.getKey()).compareTo( (K) current.getKey())>0 )) {
+			previous = current;
+			current = current.getNext();
+		}
+		if(previous == null) {
+			this.head = newNode;
+		}else {
+			previous.setNext(newNode);
+		}
+		newNode.setNext(current);
+	}
+	
+	public void adjustTail() {
+        INode tempNode=this.head;
+        while(!(tempNode==null)) {
+            this.tail=tempNode;
+            tempNode=tempNode.getNext();
+        }
+    }
+	
+	public void printMyNodes() {
+		StringBuffer myNodes = new StringBuffer("My Nodes: ");
+		INode tempNode = head;
+		while (tempNode.getNext() != null) {
+			myNodes.append(tempNode.getKey());
+			if (!tempNode.getNext().equals(null))
+				myNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getKey());
+		System.out.println(myNodes);
 	}
 }
+
+
 
