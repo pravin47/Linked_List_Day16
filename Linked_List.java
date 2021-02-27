1ae4f4fda0917fb6e9dab06f24d7350dadea2b9c
pravin bagul <bagul.pravin2@gmail.com>
2021-02-27 18:41:35 +0530

    Create_Stack

--- Linked_List.java
+++ Linked_List.java
@@ -1,19 +1,33 @@
-import org.junit.Assert;
-import org.junit.Test;
-
-public class MyQueueTest {
-	@Test
-	public void given3Numbers_WhenAddedToQueue_ShouldHaveLastAddedNode() {
-		MyQueue myQueue = new MyQueue();
-		MyNode<Integer> myFirstNode = new MyNode<>(56);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		myQueue.enqueue(myFirstNode);
-		myQueue.enqueue(mySecondNode);
-		myQueue.enqueue(myThirdNode);
-		INode<Integer> myNode = myQueue.peak();
-		myQueue.printQueue();
-		Assert.assertEquals(myFirstNode, myNode);
+public class MyStack {
+	private final MyLinkedList myLinkedList;
+	static int count = 0; 
+	public MyStack() {
+		this.myLinkedList = new MyLinkedList();
+	}
+	
+	public void push(INode myNode) {
+		myLinkedList.add(myNode);
+		count = count +1;
+	}
+	
+	public void printStack() {
+		myLinkedList.printMyNodes();
+	}
+	
+	public INode peak() {
+		return myLinkedList.head;
+	}
+	
+	public INode pop() {
+		count = count -1;
+		return myLinkedList.pop();
+	}
+	
+	public void popTillEmpty() {
+		while(count>0) {
+			myLinkedList.pop();
+			count = count -1;
+		}
 	}
 }
 
